f444f7b7ae078fe0ec0cae49ba481059ca33040e
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-06-02 10:09:52 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+          /*  int a= 6;
+            int b=9;
+        System.out.println("Hello World " + "Resultado: " + (a+b));*/
+    }
+}
